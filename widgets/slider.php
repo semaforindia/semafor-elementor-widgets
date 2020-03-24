0c14d01f07132188f9ec641635649ba4e34a2f78
Meer Waqar Ali <m.waqar@semafor.co.in>
2020-03-24 13:05:13 +0530

    Slider responsive fixes

--- widgets/slider.php
+++ widgets/slider.php
@@ -1633,7 +1633,23 @@ class Slider extends \Elementor\Widget_Base
 
 									default:
 										echo '"linear"';
-								} ?>
+								} ?>,
+			"responsiveClass":true,
+			"responsive":{
+				"0":{
+					"items":1,
+					"nav":true
+				},
+				"600":{
+					"items":3,
+					"nav":false
+				},
+				"1000":{
+					"items":5,
+					"nav":true,
+					"loop":false
+				}
+			}
 								
 
 			
